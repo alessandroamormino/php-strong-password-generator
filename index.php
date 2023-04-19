94ed4c01dd3d81aa123cd3b85d99cb3cd57ca89d
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-19 15:05:03 +0200

    feat: end milestone 2

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ X Milestone 1
 Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 Scriviamo tutto (logica e layout) in un unico file index.php
 
-Milestone 2
+X Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
 
 Milestone 3 (BONUS)
